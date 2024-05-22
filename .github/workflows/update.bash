ffa7c2749bdd45fca7e41c03a65e97ec6d0e6402
Sam Huddart <sam02h.huddart@gmail.com>
2024-05-22 11:38:12 +0100

    Update update.bash

--- .github/workflows/update.bash
+++ .github/workflows/update.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-latest_version = $(curl.exe https://api.github.com/repos/SteamDatabase/GameTracking-CS2/commits/master | jq ".sha" -r)
+latest_version = $(curl https://api.github.com/repos/SteamDatabase/GameTracking-CS2/commits/master | jq ".sha" -r)
 current_version = $(cat ./hash.txt)
 
 if [latest_version <> current_version ] then
     mkdir working/
-    curl.exe https://codeload.github.com/SteamDatabase/GameTracking-CS2/zip/refs/heads/master -O ./working/repo.zip
+    curl https://codeload.github.com/SteamDatabase/GameTracking-CS2/zip/refs/heads/master -O ./working/repo.zip
     unzip ./working/repo.zip -d ./repo/
 
     rm -rf ./protobufs/
