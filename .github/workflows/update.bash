242fa10a97081c8819077f06a5a25dbed06721b8
Sam Huddart <sam02h.huddart@gmail.com>
2024-05-22 11:39:31 +0100

    Update update.bash

--- .github/workflows/update.bash
+++ .github/workflows/update.bash
@@ -3,7 +3,12 @@
 latest_version = $(curl https://api.github.com/repos/SteamDatabase/GameTracking-CS2/commits/master | jq ".sha" -r)
 current_version = $(cat ./hash.txt)
 
+echo "Current Version: ${current_version}"
+echo "Latest Version: ${latest_version}"
+
 if [latest_version <> current_version ] then
+    echo "Updating..."
+
     mkdir working/
     curl https://codeload.github.com/SteamDatabase/GameTracking-CS2/zip/refs/heads/master -O ./working/repo.zip
     unzip ./working/repo.zip -d ./repo/
@@ -13,7 +18,9 @@ if [latest_version <> current_version ] then
     cp ./working/repo/Protobufs/* ./protobufs/
 
     cat ./working/repo/game/csgo/steam.inf > ./steam.inf
-    echo latest_version > ./hast.txt
+    echo latest_version > ./hash.txt
+
+    rm -rf ./working/
 
     git add -A
     git commit -m "Update Mirror to ${latest_version}"
